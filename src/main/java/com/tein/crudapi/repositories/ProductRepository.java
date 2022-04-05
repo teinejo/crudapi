262c1a818fda06e72e7e48c7219ecd795c067387
teinejo <80319351+teinejo@users.noreply.github.com>
2022-04-05 15:40:51 +0300

    Revert "base arch commit"
    This reverts commit a588c02df3d594b7e2d02c944bd6bfa72d244b6c.

--- src/main/java/com/tein/crudapi/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tein.crudapi.repositories;
-
-import com.tein.crudapi.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-    Product findByName(String name);
-
-}
